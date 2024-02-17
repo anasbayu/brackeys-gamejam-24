a903d530d5c3a3fca0507351153e85f01bef8a9c
arshavyna <anasbayu03@gmail.com>
2024-02-18 02:04:29 +0700

    add: main menu animation

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -4,14 +4,28 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour{
-    public GameObject mCreditSection;
+    public GameObject mHintSection;
+    public GameObject mMenuSectionToHide;
+    bool isReadyToStart = false;
+
+    void Start(){
+        Time.timeScale = 1f;
+    }
+
+    void Update(){
+        if(isReadyToStart && Input.GetKey(KeyCode.Space)){
+            Load("Gameplay");
+        }
+    }
 
     public void Load(string sceneName){
         SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
     }
 
-    public void ShowCredits(){
-        // mCreditSection.GetComponent<Animator>().Play();
+    public void ShowHint(){
+        isReadyToStart = true;
+        mMenuSectionToHide.SetActive(false);
+        mHintSection.GetComponent<Animator>().SetBool("ScrollDown", true);
     }
     public void QuitGame(){
         Application.Quit();
